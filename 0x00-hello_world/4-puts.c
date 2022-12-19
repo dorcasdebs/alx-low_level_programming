b839448250af0d7ad6e39d4707d4acba139ba07f
Dorcas <11181980dorcieee@gmail.com>
2022-12-19 06:05:26 -0800

    tasks 4 to 8

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description - prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Description:prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0);
+}
+
