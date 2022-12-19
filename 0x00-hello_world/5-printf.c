fe0ebd67b5deb500cb6f46437cf8a7b1c62839b7
Dorcas <11181980dorcieee@gmail.com>
2022-12-19 06:24:18 -0800

    task 5 correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 /**
- * main- entry point
- *
- * Description:prints "with proper grammar, but the outcome is a piece of art,"
- *
+ * main - entry point
  * Return: always 0
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	Return(0);
+	return (0);
 }
 
