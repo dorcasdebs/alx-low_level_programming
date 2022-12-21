e10090d099e7a9d2df72014db349f70bff510315
Dorcas <11181980dorcieee@gmail.com>
2022-12-20 16:07:02 -0800

    updated main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
+/**
+ * int prototypes()
+ * void prototypes()
+ */
 
 0. void reset_to_98(int *n);
 
@@ -25,3 +28,5 @@
 10. int _atoi(char *s);
 
 11. int _putchar(char c);
+
+#endif
